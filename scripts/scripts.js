3d3dcbccc21066b588c0e55d4346467e2e8b04e9
Rafael <vg_rafael2@hotmail.com>
2015-11-24 11:36:18 -0200

    Update scripts.js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -129,9 +129,10 @@ function init_game() {
 		if (rect.y * 2 < 400){
 			player_y = rect.y * 2;
 		}
+		if(connection==true{})
 		var channel = new RTCMultiSession();
 		channel.send("x: " + player_x / 2 + " y: " + player_y / 2);
-		
+		}
 		player.css({ left: player_x, top: player_y });
       });
     }});
